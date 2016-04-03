9b98a9084720cb4c4d20db1bd986714a5d3e2914
Samuel Friedman <sifriedm@asu.edu>
2016-04-02 17:19:40 -0700

    Week 0.1
    Website as it is after an initial setup on 4/2/2016.

---
+++ colors.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Unsung Colors</title>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	
+	<body>
+		<table>
+			<tr>
+				<td style="width: 25%;"><input type="button" class="btn-g1" value = "Gold 1 Button"/></td>
+				<td style="width: 25%;"><input type="button" class="btn-g2" value = "Gold 2 Button"/></td>
+				<td style="width: 25%;"><input type="button" class="btn-g3" value = "Gold 3 Button"/></td>
+				<td style="width: 25%;"><input type="button" class="btn-g4" value = "Gold 4 Button"/></td>
+			</tr>		
+		</table>
+	</body>
+</html>
